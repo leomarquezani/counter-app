38c2c94c01484751a780e7e08eb329fe4c02ee43
Leo <leomarquezani@gmail.com>
2018-11-14 00:02:06 -0200

    added delete and reset

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export default class Counter extends Component {
     state = {
-        count: 0,
+        value: this.props.counter.value,
         tags: ['tag1', 'tag2', 'tag3']
     };
 
@@ -15,7 +15,7 @@ export default class Counter extends Component {
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button onClick={() => this.handleIncrement(1)} className="btn btn-secondary btn-sm">Increment</button>
-                { this.renderTags()}
+                <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </div>
         );
     }
@@ -31,16 +31,16 @@ export default class Counter extends Component {
 
     getBadgeClasses() {
         let classes = "badge m-2 ";
-        classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+        classes += this.state.value === 0 ? "badge-warning" : "badge-primary";
         return classes;
     }
 
     formatCount() {
-        return this.state.count === 0 ? 'Zero' : this.state.count;
+        return this.state.value === 0 ? 'Zero' : this.state.value;
     }
 
     handleIncrement = (product) => {
-        console.log(product);
-        this.setState({ count: this.state.count + 1});
+        console.log(this.props);
+        this.setState({ value: this.state.value + 1});
     }
 }
